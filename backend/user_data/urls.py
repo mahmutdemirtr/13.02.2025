a39ccf9303769c514fee6be541df91c46fd7a26b
EC2 Default User <ec2-user@ip-172-31-20-137.ec2.internal>
2025-02-25 19:46:08 +0000

    updated

--- backend/user_data/urls.py
+++ backend/user_data/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import SurveyViewSet, EmailViewSet, StoreUsername, ScrapeData, create_checkout_session, stripe_webhook
+from .views import SurveyViewSet, EmailViewSet, StoreUsername, ScrapeData
 
 router = DefaultRouter()
 router.register(r'emails', EmailViewSet, basename='email')
@@ -9,7 +9,4 @@ router.register(r'surveys', SurveyViewSet, basename='survey')
 urlpatterns = [
     path('store_username/', StoreUsername.as_view(), name='store_username'),
     path('scrape/', ScrapeData.as_view(), name='scrape_data'),
-    path('create-checkout-session/', create_checkout_session, name="create_checkout_session"),
-    path('', include(router.urls)),
-    path("webhook/", stripe_webhook, name="stripe_webhook"),
 ]
