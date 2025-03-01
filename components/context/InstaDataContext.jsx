bee6688ede3357a8dc8aed3c7dfa2cda3064d34a
mahmutdemirtr <140492186+mahmutdemirtr@users.noreply.github.com>
2025-03-01 14:24:16 +0300

    return every think on Update InstaDataContext.jsx to beni ubdates

--- components/context/InstaDataContext.jsx
+++ components/context/InstaDataContext.jsx
@@ -7,10 +7,20 @@ const InstaDataContext = createContext()
 export const InstaDataProvider = (({ children }) => {
     const [userData, setUserData] = useState({});
     const [username, setUsername] = useState('');
+    const [isPaid, setIsPaid] = useState(false);
+    const [isUnlocked, setIsUnlocked] = useState(false);
 
     return (
-        <InstaDataContext.Provider value={{ userData, setUserData, username, setUsername }}>
-
+        <InstaDataContext.Provider value={{
+            userData,
+            setUserData,
+            username,
+            setUsername,
+            isPaid,
+            setIsPaid,
+            isUnlocked,
+            setIsUnlocked
+        }}>
             {children}
         </InstaDataContext.Provider>
     )
