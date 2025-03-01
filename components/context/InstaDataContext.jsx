261e2cc2d0a3762f2a595857b1c309924c0b71a3
mahmutdemirtr <140492186+mahmutdemirtr@users.noreply.github.com>
2025-03-01 13:59:09 +0300

    return the InstaDataContext.jsx to old version

--- components/context/InstaDataContext.jsx
+++ components/context/InstaDataContext.jsx
@@ -7,20 +7,10 @@ const InstaDataContext = createContext()
 export const InstaDataProvider = (({ children }) => {
     const [userData, setUserData] = useState({});
     const [username, setUsername] = useState('');
-    const [isPaid, setIsPaid] = useState(false);
-    const [isUnlocked, setIssUnlocked] = useState(false);
 
     return (
-        <InstaDataContext.Provider value={{
-            userData,
-            setUserData,
-            username,
-            setUsername,
-            isPaid,
-            setIsPaid,
-            isUnlocked,
-            setIssUnlocked
-        }}>
+        <InstaDataContext.Provider value={{ userData, setUserData, username, setUsername }}>
+
             {children}
         </InstaDataContext.Provider>
     )
